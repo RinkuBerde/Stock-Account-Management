2882ce5c55c26fc933edf44e0028682c0479a0d7
rinkuberde <rinkuberde@gmail.com>
2023-02-11 21:43:59 +0530

    [Rinku] -Calculate the value of each stock and the total value

--- Stock-Account-Management/Program.cs
+++ Stock-Account-Management/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Stock_Account_Management
+﻿using Newtonsoft.Json;
+
+namespace Stock_Account_Management
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Stock-Account-Management!!!");
+            //Creating obj for StockManager
+            StockManager stockManager = new StockManager();
+            //getting path of json file
+            string file = @"C:\Users\RINKU\Desktop\Day6\Stock-Account-Management\Stock-Account-Management\Stock.json";
+            //DeserializeO Json file
+            StockUtility stockUtility = JsonConvert.DeserializeObject<StockUtility>(File.ReadAllText(file));
+            Console.WriteLine("Enter which operation to perform\n 1-Add a stock\n 2-Remove a stock\n 3-Display Stocks");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var fs = stockUtility.stocksList;
+            switch (num)
+            {
+                case 1:
+                    stockManager.AddStock(fs);
+                    File.WriteAllText(file, JsonConvert.SerializeObject(stockUtility));
+                    stockManager.DisplayStocks(fs);
+                    break;
+                case 2:
+                    stockManager.DeleteInventory(fs);
+                    File.WriteAllText(file, JsonConvert.SerializeObject(stockUtility));
+                    stockManager.DisplayStocks(fs);
+                    break;
+                case 3:
+                    stockManager.DisplayStocks(fs);
+                    break;
+            }
         }
     }
 }
---
+++ Stock-Account-Management/StockUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Account_Management
+{
+    class StockUtility
+    {
+        public List<Stocks> stocksList { get; set; }
+        public class Stocks
+        {
+            public string StockName { get; set; }
+            public int shares { get; set; }
+            public int Price { get; set; }
+        }
+    }
+}
