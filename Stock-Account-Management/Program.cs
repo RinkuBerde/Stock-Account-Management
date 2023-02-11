1d9810b8a2c85c11bf34359a2f277f0799613cb7
rinkuberde <rinkuberde@gmail.com>
2023-02-11 21:28:01 +0530

    [Rinku] -Welcome Message

---
+++ Stock-Account-Management/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stock_Account_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Stock-Account-Management!!!");
+        }
+    }
+}
